246ce34e7874703eb05673a8bedc870e9f170116
Raymond Li <rlii0089@student.monash.edu>
2024-05-10 02:00:48 +1000

    Commit 3

--- DrinkFilterTableViewController.swift
+++ DrinkFilterTableViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class DrinkFilterTableViewController: UITableViewController {
 
-    var ingredient: String?
-    var cocktails: [Cocktail] = []
+    weak var delegate: DrinkFilterDelegate?
+    var filterOptions: [String] = []
+    var selectedFilters: [String] = []
+    var filterType: String = ""
+
+    private let cocktailBaseURL = "https://www.thecocktaildb.com/api/json/v1/1"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,42 +25,159 @@ class DrinkFilterTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
 
-        if let ingredient = ingredient {
-            NetworkManager.shared.fetchCocktailsByIngredient(ingredient: ingredient) { [weak self] cocktails, error in
-                guard let self = self else { return }
-                
-                if let error = error {
-                    print("Error fetching cocktails: \(error.localizedDescription)")
-                } else if let cocktails = cocktails {
-                    self.cocktails = cocktails
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-            }
-        }
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FilterCell")
+        tableView.allowsMultipleSelection = true
+        
+        fetchFilterOptions()
     }
 
+    @IBAction func ApplyButton(_ sender: Any) {
+        delegate?.didSelectFilters(filters: selectedFilters, filterType: filterType)
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return cocktails.count
+        return filterOptions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CocktailCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FilterCell", for: indexPath)
         
-        let cocktail = cocktails[indexPath.row]
-        cell.textLabel?.text = cocktail.strDrink
-        cell.detailTextLabel?.text = cocktail.strCategory
+        cell.textLabel?.text = filterOptions[indexPath.row]
+        
+        if selectedFilters.contains(filterOptions[indexPath.row]) {
+            tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+            cell.accessoryType = .checkmark
+        } else {
+            tableView.deselectRow(at: indexPath, animated: true)
+            cell.accessoryType = .none
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedOption = filterOptions[indexPath.row]
+        if !selectedFilters.contains(selectedOption) {
+            selectedFilters.append(selectedOption)
+        }
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let deselectedOption = filterOptions[indexPath.row]
+        if let index = selectedFilters.firstIndex(of: deselectedOption) {
+            selectedFilters.remove(at: index)
+        }
+        tableView.reloadData()
+    }
+    
+    func fetchFilterOptions() {
+        switch filterType {
+        case "Category":
+            fetchCategories()
+        case "Glass":
+            fetchGlasses()
+        case "Ingredient":
+            fetchIngredients()
+        default:
+            break
+        }
+    }
+    
+    func fetchCategories() {
+        let categoryURL = "\(cocktailBaseURL)/list.php?c=list"
+
+        if let url = URL(string: categoryURL) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        if let dictionary = json as? [String: Any], let categories = dictionary["drinks"] as? [[String: String]] {
+                            for category in categories {
+                                if let categoryName = category["strCategory"] {
+                                    self.filterOptions.append(categoryName)
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
+
+        tableView.reloadData()
+    }
+    
+    func fetchGlasses() {
+        
+        let glassURL = "\(cocktailBaseURL)/list.php?g=list"
+
+        if let url = URL(string: glassURL) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        if let dictionary = json as? [String: Any], let glasses = dictionary["drinks"] as? [[String: String]] {
+                            for glass in glasses {
+                                if let glassName = glass["strGlass"] {
+                                    self.filterOptions.append(glassName)
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
+
+        tableView.reloadData()
+    }
+    
+    func fetchIngredients() {
+        
+        let ingredientURL = "\(cocktailBaseURL)/list.php?i=list"
+
+        if let url = URL(string: ingredientURL) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        if let dictionary = json as? [String: Any], let ingredients = dictionary["drinks"] as? [[String: String]] {
+                            for ingredient in ingredients {
+                                if let ingredientName = ingredient["strIngredient1"] {
+                                    self.filterOptions.append(ingredientName)
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
+
+        tableView.reloadData()
     }
     
 
--- DrinkIngredientsFilterViewController.swift
+++ DrinkIngredientsFilterViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
-class DrinkIngredientsFilterViewController: UIViewController {
+class DrinkIngredientsFilterViewController: UIViewController, DrinkFilterDelegate {
+    
+    var selectedCategoryFilters: [String] = []
+    var selectedGlassFilters: [String] = []
+    var selectedIngredientFilters: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +19,51 @@ class DrinkIngredientsFilterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var CategoryButton: UIButton!
+    @IBOutlet weak var GlassButton: UIButton!
+    @IBOutlet weak var IngredientButton: UIButton!
+    
+    @IBAction func CategoryButtonTapped(_ sender: Any) {
+        showFilterOptions(for: "Category")
+    }
+    
+    @IBAction func GlassButtonTapped(_ sender: Any) {
+        showFilterOptions(for: "Glass")
+    }
+    
+    @IBAction func IngredientButtonTapped(_ sender: Any) {
+        showFilterOptions(for: "Ingredient")
+    }
+    
+    func showFilterOptions(for filterType: String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let filterVC = storyboard.instantiateViewController(withIdentifier: "DrinkFilterTableViewController") as? DrinkFilterTableViewController else {
+            return
+        }
+        
+        filterVC.delegate = self
+        
+        filterVC.filterType = filterType
+                
+        navigationController?.pushViewController(filterVC, animated: true)
+    }
+    
+    func didSelectFilters(filters: [String], filterType: String) {
+        switch filterType {
+        case "Category":
+            selectedCategoryFilters = filters
+            CategoryButton.setTitle("Category (\(filters.count))", for: .normal)
+        case "Glass":
+            selectedGlassFilters = filters
+            GlassButton.setTitle("Glass (\(filters.count))", for: .normal)
+        case "Ingredient":
+            selectedIngredientFilters = filters
+            IngredientButton.setTitle("Ingredient (\(filters.count))", for: .normal)
+        default:
+            break
+        }
+   }
+    
     /*
     // MARK: - Navigation
 
