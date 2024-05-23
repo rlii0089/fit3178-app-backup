245be46911e1dbf45f0b74d99a7a09a496d9b345
Raymond Li <rlii0089@student.monash.edu>
2024-05-23 16:36:01 +1000

    Updated files

--- shake-and-serve/Core/Generate/Food/GeneratedFoodViewController.swift
+++ shake-and-serve/Core/Generate/Food/GeneratedFoodViewController.swift
@@ -15,7 +15,11 @@ class GeneratedFoodViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var foodNameLabel: UILabel!
+    @IBOutlet weak var foodCategoryLabel: UILabel!
+    @IBOutlet weak var foodAreaLabel: UILabel!
+    
     /*
     // MARK: - Navigation
 
